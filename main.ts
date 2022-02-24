a367f7dbeba19ce68b44b368c930e89c51ba6ebe
趙健傑 <100336478+nightgod0622@users.noreply.github.com>
2022-02-24 14:43:46 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+})
